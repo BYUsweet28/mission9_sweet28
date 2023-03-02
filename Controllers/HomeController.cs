5d40cb1954112aa6a6b202e7a337aeba5b296655
BYUsweet28 <sweet28@byu.edu>
2023-03-01 21:43:57 -0700

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using BookStore.Models;
+using BookStore.Models.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStore.Controllers
+{
+    public class HomeController : Controller
+    {
+        private IBookStoreRepository repo;
+        private BookStoreContext context { get; set; }
+        public HomeController(IBookStoreRepository temp)
+        {
+            repo = temp;
+        }
+        public IActionResult Index(int pageNum = 1)
+        {
+            int pageSize = 10;
+
+            var x = new BooksViewModel
+            {
+                Book = repo.book
+                .OrderBy(p => p.Title)
+                .Skip((pageNum - 1) * pageSize)
+                .Take(pageSize),
+
+                PageInfo = new PageInfo
+                {
+                    TotalNumProjects = repo.book.Count(),
+                    ProjectsPerPage = pageSize,
+                    CurrentPage = pageNum
+                }
+            };
+
+                return View(x);
+        }
+    }
+}
